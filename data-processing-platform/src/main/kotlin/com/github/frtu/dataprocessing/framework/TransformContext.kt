9de136060f3c24c576f6fe5f31dbcbe24565e2e1
Tu, Fred <frederic.tu@gmail.com>
2025-12-15 07:34:51 +0800

    feat: Adding ability to retrieve optional metadata from Context

--- data-processing-platform/src/main/kotlin/com/github/frtu/dataprocessing/framework/TransformContext.kt
+++ data-processing-platform/src/main/kotlin/com/github/frtu/dataprocessing/framework/TransformContext.kt
@@ -4,3 +4,19 @@ interface TransformContext {
     /** Allow to get the output data from the specified step name */
     fun <T> getData(stepName: String): T
 }
+
+data class Metadata(
+    val sourceType: SourceType,
+    val operationType: OperationType,
+)
+
+enum class SourceType {
+    DEBEZIUM,
+    APPLICATION,
+}
+
+enum class OperationType {
+    CREATE,
+    UPDATE,
+    DELETE,
+}
